70f8123c38dcec7a898c49e066771bab9ccba99b
Mtausz <kontakt@mateusz-szopa.pl>
2017-04-14 16:00:28 +0100

    add cache , diet creator and  product list filtering

--- eTrener/eTrener/Controllers/DietController.cs
+++ eTrener/eTrener/Controllers/DietController.cs
@@ -5,15 +5,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using eTrener.DAL;
+using eTrener.Infrastructure;
 using eTrener.Models;
 
 namespace eTrener.Controllers
 {
     public class DietController : Controller
     {
-        private eTrenerContext db= new eTrenerContext();
+        private eTrenerContext db = new eTrenerContext();
         // GET: Diet
-    
+
         public ActionResult AddProduct()
         {
             return View();
@@ -22,7 +23,6 @@ namespace eTrener.Controllers
         [HttpPost]
         public ActionResult AddProduct(ProductModel model)
         {
-
             ProductModel models = new ProductModel
             {
                 ProductId = model.ProductId,
@@ -36,12 +36,42 @@ namespace eTrener.Controllers
             db.SaveChanges();
             return RedirectToAction("AddProduct", "Diet");
         }
-        public ActionResult ProductList()
+
+        public ActionResult ProductList(string searchQuery = null)
+        {
+            List<ProductModel> productList;
+          //  ICacheProvider cache = new CacheProvider();
+         //   if (cache.IsSet(Consts.ProductList))
+         //   {
+         //       productList = cache.Get(Consts.ProductList) as List<ProductModel>;
+        //    }
+        //    else
+//{
+                productList = db.Products.Where(a => (searchQuery == null
+                ||a.Name.ToLower().Contains(searchQuery.ToLower())
+                )).ToList();
+            //      cache.Set(Consts.ProductList, productList, 10);
+            //    }
+            if (Request.IsAjaxRequest())
+            {
+                return PartialView("_Products", productList);
+            }
+            return View(productList);
+        }
+
+        public ActionResult CreateDiet()
         {
             var list = db.Products.ToList();
             return View(list);
         }
 
+        public ActionResult ProductHints(string term)
+        {
+            var productList =
+                db.Products.Where(a => a.Name.ToLower().Contains(term.ToLower()))
+                    .Take(5)
+                    .Select(a => new {label = a.Name});
+            return Json(productList, JsonRequestBehavior.AllowGet);
+        }
     }
-
 }
